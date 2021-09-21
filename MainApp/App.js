edb96c66205062f04cd711990d4a77bf7dc70224
Nuttakorn Tedthong <leendsru@hotmail.com>
2021-09-21 14:23:58 +0700

    List, ScrollView & RefreshControl

--- MainApp/App.js
+++ MainApp/App.js
@@ -15,6 +15,8 @@ import {
   Text,
   useColorScheme,
   View,
+  ScrollView,
+  RefreshControl,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
@@ -46,65 +48,51 @@ const Section = ({children, title}): Node => {
 };
 
 const App: () => Node = () => {
+  const [items, setItems] = useState([
+    {key: 1, item: 'Item 1'},
+    {key: 2, item: 'Item 2'},
+    {key: 3, item: 'Item 3'},
+    {key: 4, item: 'Item 4'},
+    {key: 5, item: 'Item 5'},
+    {key: 6, item: 'Item 6'},
+    {key: 7, item: 'Item 7'},
+    {key: 8, item: 'Item 8'},
+  ]);
+
   const isDarkMode = useColorScheme() === 'dark';
-  const [name, setName] = useState('Leen');
-  const [session, setSession] = useState({number: 6, title: 'Nuttakorn'});
-  const [current, setCurrent] = useState(true);
-
-  const [countClick, setCountClick] = useState(0);
-  const [countNumber, setCountNumber] = useState(0);
-
-  const onClickHandle = () => {
-    setName('Programming with Leen');
-    setSession({number: 7, title: 'ShAkESpEAr'});
-    setCurrent(false);
-  };
-
-  const onAddHandle = () => {
-    setCountNumber(prevCount => {
-      return (prevCount += 5);
-    });
-    setCountClick(prevState => prevState + 1);
-  };
-
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setItems([...items, {key: 213, item: 'Leen'}]);
+      setRefreshing(false);
+    }, 2000);
+  };
+
   return (
     <View style={styles.body}>
-      <View style={styles.viewTop}>
-        <View style={styles.view1}>
-          <Text style={styles.text}>1</Text>
-        </View>
-
-        <View style={styles.view2}>
-          <Text style={styles.text}>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text style={styles.text}>3</Text>
-        </View>
-      </View>
-
-      <View style={styles.viewMiddle}>
-        <View style={styles.view4}>
-          <Text style={styles.text}>4</Text>
-        </View>
-      </View>
-      <View style={styles.viewMiddle}>
-        <View style={styles.view5}>
-          <Text style={styles.text}>5</Text>
-        </View>
-      </View>
-
-      <View style={styles.viewBottom}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>7</Text>
-        </View>
-      </View>
+      <ScrollView
+        horizontal={false}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={['#ff00ff']}
+          />
+        }>
+        {items.map(object => {
+          return (
+            <View style={styles.item} key={object.key}>
+              <Text style={styles.text}>{object.item}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 };
@@ -114,69 +102,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff54f',
     flexDirection: 'column',
-    alignItems: 'stretch', // Center Horizontally
-    justifyContent: 'flex-start', // Center Vertically
   },
-
-  viewTop: {
-    flexDirection: 'row',
+  item: {
+    margin: 10,
+    backgroundColor: '#34e1eb',
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  viewMiddle: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  viewBottom: {
-    flexDirection: 'row',
-    flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  view1: {
-    flex: 1,
-    backgroundColor: '#000fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view2: {
-    flex: 2,
-    backgroundColor: '#009999',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view3: {
-    flex: 3,
-    backgroundColor: '#fffabc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view4: {
-    flex: 1,
-    backgroundColor: '#ff1234',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view5: {
-    flex: 1,
-    backgroundColor: '#ff34ed',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view6: {
-    flex: 1,
-    backgroundColor: '#fedf12',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view7: {
-    flex: 1,
-    backgroundColor: '#15456f',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
   text: {
     color: '#000000',
     fontSize: 20,
@@ -184,13 +116,5 @@ const styles = StyleSheet.create({
     margin: 10,
   },
 });
-
-const styles2 = StyleSheet.create({
-  body2: {
-    backgroundColor: '#000000',
-  },
-});
-
-const container = StyleSheet.compose(styles.body, styles2.body2);
 
 export default App;
