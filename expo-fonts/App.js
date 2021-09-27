6ec882988a0bea344d03ba1fd233a0147793a451
Nuttakorn Tedthong <leendsru@hotmail.com>
2021-09-27 09:00:15 +0700

    add expo fonts to remote repository

---
+++ expo-fonts/App.js
@@ -0,0 +1,44 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { ArchitectsDaughter_400Regular } from "@expo-google-fonts/architects-daughter";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+
+export default function App() {
+  let [fontLoaded, error] = useFonts({
+    Regular: ArchitectsDaughter_400Regular,
+    Kanit: require("./assets/fonts/Kanit-Regular.ttf"),
+  });
+
+  //
+  //expo install @expo-google-fonts/architects-daughter https://directory.vercel.app/
+  //expo install expo-app-loading
+
+  if (!AppLoading) {
+    return <AppLoading />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontFamily: "Kanit",
+          fontSize: 20,
+        }}
+      >
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
